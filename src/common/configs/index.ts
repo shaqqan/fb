4bab50d908a4ba324869abbe5499364bdf5fb802
a_berdimuratov <A.Berdimuratov@uzinfocom.uz>
2025-08-16 23:04:42 +0500

    Refactor i18n configuration: update app.module.ts to use async loading with ConfigService, add I18nConfig for fallback and locale settings, and remove unused load-esm dependency from package-lock.json.

--- src/common/configs/index.ts
+++ src/common/configs/index.ts
@@ -2,3 +2,4 @@ export { default as appConfig } from './app.config';
 export { default as jwtConfig } from './jwt.config';
 export { default as typeormConfig } from './typeorm.config';
 export { default as redisConfig } from './redis.config';
+export { I18nConfig } from './i18n.config';
