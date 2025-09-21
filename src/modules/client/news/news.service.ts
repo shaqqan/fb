a8973e64bab4b54afbfae3cb95066e7cfacf24cf
a_berdimuratov <A.Berdimuratov@uzinfocom.uz>
2025-09-21 11:07:51 +0500

    Refactor import path for currentLocale in NewsService to improve code organization.

--- src/modules/client/news/news.service.ts
+++ src/modules/client/news/news.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { News, NewsStatus } from 'src/databases/typeorm/entities';
 import { PaginateQuery, Paginated, paginate } from 'nestjs-paginate';
-import { currentLocale } from 'src/helpers';
+import { currentLocale } from 'src/common/utils';
 
 @Injectable()
 export class NewsService {
